60ae24d1ec2a5d0ceff3ca7449c78afbdd81f212
Augustocif99 <56948590+Augustocif99@users.noreply.github.com>
2020-04-02 22:14:29 -0600

    clase TipoProducto

---
+++ webapp/POS/src/java/org/models/TipoProducto.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.models;
+
+/**
+ *
+ * @author josea
+ */
+public class TipoProducto {
+    private int idTipoProducto;
+    private String descripcion;
+    private int estado;
+
+    public int getIdTipoProducto() {
+        return idTipoProducto;
+    }
+
+    public void setIdTipoProducto(int idTipoProducto) {
+        this.idTipoProducto = idTipoProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
