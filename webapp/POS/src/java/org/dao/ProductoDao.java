407f6fad3ea3f333550a6aeeedf92a15d01c7d74
javier <javierjavieryes@gmail.com>
2020-04-06 23:20:24 -0600

    conexion y dao de productos

--- webapp/POS/src/java/org/dao/ProductoDao.java
+++ webapp/POS/src/java/org/dao/ProductoDao.java
@@ -5,16 +5,26 @@
  */
 package org.dao;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.interfaces.ProductoInterface;
 import org.models.Producto;
-
+import org.utils.Conexion;
 /**
  *
  * @author javie
  */
 public class ProductoDao implements ProductoInterface{
-
+    
+    private boolean resultadoDB = false;
+    private String querySql ="";
+    private ResultSet resulSet = null;
+    private  Conexion conec = new Conexion();
+    
+    
     @Override
     public boolean agregarProducto(Producto producto) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -22,8 +32,42 @@ public class ProductoDao implements ProductoInterface{
 
     @Override
     public List<Producto> listarProducto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        List<Producto> listaProductos = new ArrayList();
+        this.querySql = "SELECT * FROM POS.PRODUCTO";
+        
+        try {
+            this.conec.open();
+            this.resulSet = this.conec.executeQuery(querySql);
+            this.conec.close();
+            this.resulSet.close();
+            
+            while(this.resulSet.next()){
+            Producto producto = new Producto();
+            
+            producto.setId_producto(this.resulSet.getInt("ID_PRODUCTO"));
+            producto.setId_Tipo_Producto(this.resulSet.getInt("ID_TIPO_PRODUCTO"));
+            producto.setDescripcion(this.resulSet.getString("DESCRIPCION"));
+            producto.setPrecio(this.resulSet.getInt("PRECIO"));
+            producto.setExistencia(this.resulSet.getInt("EXISTENCIA"));
+            producto.setEstado(this.resulSet.getInt("ESTADO"));
+            
+            
+            
+            listaProductos.add(producto);
+            }
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProductoDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(ProductoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        return listaProductos;
+        
+         }
 
     @Override
     public boolean editarProducto(Producto producto) {
---
+++ webapp/POS/src/java/org/utils/Conexion.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author javie
+ */
+public class Conexion {
+    
+    
+      private PreparedStatement preparar = null;    
+    private  Connection coneccion=null;    
+    private ResultSet resultado = null;
+     //Cadena de Conexion
+    String stringConnectionUrl = "jdbc:sqlserver://db-umg.cowbs9fgq896.us-east-1.rds.amazonaws.com,1433:1433;" +
+                                "databaseName=POS;";
+    //Driver o controlador JDBC
+    String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    
+    public Connection open() throws ClassNotFoundException{        
+       
+          System.out.println("si abrio ");
+        try {  
+                Class.forName(driver);
+                coneccion = DriverManager.getConnection(stringConnectionUrl,"admin","developer2020");
+              
+                
+        } catch (SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);            
+            System.out.println("Excepción: " + e.getMessage());
+        }
+        return coneccion;
+    }
+    
+    public void close() throws Exception{
+        //Connection coneccion = null;
+        try {
+            if (coneccion != null){
+                coneccion.clearWarnings();
+                coneccion.close();                
+            }
+        } catch (SQLException e) {
+            coneccion = null;
+            throw new Exception(e.getMessage());
+        }
+
+    }
+    
+    public boolean executeSql (String cmd) throws Exception{
+            if (cmd != null)
+            try {
+                    this.preparar = this.coneccion.prepareStatement(cmd);
+                    this.preparar.executeUpdate();
+            } catch (SQLException e) {
+                    throw new Exception(e.getMessage());
+            }
+            else
+                    throw new Exception("El comando a ejecutar es nulo!");
+            return true;
+    }
+	
+    public ResultSet executeQuery (String strSQL){
+        
+            if (strSQL != null)
+            try {        
+                    
+                    preparar = coneccion.prepareStatement(strSQL);                    
+                    resultado = preparar.executeQuery();	
+            } catch (SQLException e) {                    
+                System.out.println("Error al ejecutar el query en Clase: Conexion: " + e.toString());
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+            }
+            //close();
+            return resultado;
+    }
+    
+}
